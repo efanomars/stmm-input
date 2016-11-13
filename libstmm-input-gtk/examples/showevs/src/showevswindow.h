716d249eaf38388105a8029029032765e805d2c9
smarsili <smarsili@debian7.cagia.home>
2016-11-13 21:56:05 +0100

    Fix license text

--- libstmm-input-gtk/examples/showevs/src/showevswindow.h
+++ libstmm-input-gtk/examples/showevs/src/showevswindow.h
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2016  Stefano Marsili, <stemars@gmx.ch>
  *
- * This library is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
